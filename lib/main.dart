77307158a1fd270116b7c023e293d20efd0939a3
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-02-28 21:46:59 +0900

    TEST

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'dart:async';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  StreamController<String> streamController = StreamController<String>();
+
+  IO.Socket socket = IO.io(
+    'http://121.174.55.140:8080',
+    IO.OptionBuilder().setTransports(['websocket']).enableAutoConnect().build(),
+  );
+
+  _connect() {
+    socket.onConnect((data) => debugPrint('연결 성공'));
+    socket.on('test', (data) => streamController.sink.add(data));
+    socket.onDisconnect((data) => debugPrint('끊김'));
+    socket.connect();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _connect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: streamController.stream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Center(child: Text(snapshot.data.toString()));
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text("에러"),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
